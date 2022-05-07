0787c45e4616af0b3b665b6b962fbdc007c531ca
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-05-07 10:00:49 +0200

    Update kheap.c

--- FOS_PROJECT_2022_Term2_Template/kern/kheap.c
+++ FOS_PROJECT_2022_Term2_Template/kern/kheap.c
@@ -10,14 +10,12 @@
 uint32 startAdd = KERNEL_HEAP_START;
 int idx = 0;
 
-struct HEAP
-{
+struct KernelHEAP {
 	uint32 first;
 	int size;
-} heapArr[Mega];
+} kHeapArr[Mega];
 
-void* kmalloc(unsigned int size)
-{
+void* kmalloc(unsigned int size) {
 	//TODO: [PROjECT 2022 - [1] Kernel Heap] kmalloc()
 	// Write your code here, remove the panic and write your code
 	//kpanic_into_prompt("kmalloc() is not implemented yet...!!");
@@ -26,34 +24,31 @@ void* kmalloc(unsigned int size)
 	//NOTE: All kernel heap allocations are multiples of PAGE_SIZE (4KB)
 	//refer to the project presentation and documentation for details
 
-	uint32 newStart = startAdd;
-	heapArr[idx].size = ROUNDUP(size, PAGE_SIZE);
-	uint32 maxSize = startAdd+size;
+	if(isKHeapPlacementStrategyNEXTFIT()) {
+		uint32 newStart = startAdd;
+		kHeapArr[idx].size = ROUNDUP(size, PAGE_SIZE);
+		uint32 maxSize = startAdd+size;
 
-	if(KERNEL_HEAP_MAX-startAdd > size)
-	{
-		for(uint32 i = startAdd; i < maxSize; i += PAGE_SIZE)
-		{
-			struct Frame_Info* ptrFrameInfo;
-			int tmp = allocate_frame(&ptrFrameInfo);
+		if(KERNEL_HEAP_MAX-startAdd > size) {
+			for(uint32 i = startAdd; i < maxSize; i += PAGE_SIZE) {
+				struct Frame_Info* ptrFrameInfo;
+				int tmp = allocate_frame(&ptrFrameInfo);
+				if(tmp == E_NO_MEM) return NULL;
 
-			if(tmp == E_NO_MEM) return NULL;
+				tmp = map_frame(ptr_page_directory, ptrFrameInfo, (void*)i, PERM_PRESENT|PERM_WRITEABLE);
+				if(tmp == E_NO_MEM) {
+					free_frame(ptrFrameInfo);
+					return NULL;
+				}
 
-			tmp = map_frame(ptr_page_directory, ptrFrameInfo, (void*)i, PERM_PRESENT|PERM_WRITEABLE);
-
-			if(tmp == E_NO_MEM)
-			{
-				free_frame(ptrFrameInfo);
-				return NULL;
+				startAdd += PAGE_SIZE;   //Moving Forward
 			}
 
-			startAdd += PAGE_SIZE;   //Moving Forward
+			kHeapArr[idx].first = newStart;
+
+			idx++;
+			return (void*)newStart;
 		}
-
-		heapArr[idx].first = newStart;
-		idx++;
-
-		return (void*)newStart;
 	}
 
 	//TODO: [PROjECT 2022 - BONUS1] Implement a Kernel allocation strategy
@@ -64,11 +59,14 @@ void* kmalloc(unsigned int size)
 	//functions to check the current strategy
 	//change this "return" according to your answer
 
+	if(isKHeapPlacementStrategyBESTFIT()) {
+		// BEST FIT strategy
+	}
+
 	return NULL;
 }
 
-void kfree(void* virtual_address)
-{
+void kfree(void* virtual_address) {
 	//TODO: [PROJECT 2022 - [2] Kernel Heap] kfree()
 	// Write your code here, remove the panic and write your code
 	//panic("kfree() is not implemented yet...!!");
@@ -76,23 +74,19 @@ void kfree(void* virtual_address)
 	//you need to get the size of the given allocation using its address
 	//refer to the project presentation and documentation for details
 
-	for(int i=0; i<idx; i++)
-	{
-		if (virtual_address == (void*)heapArr[i].first)
-		{
-			uint32 finalAdd = heapArr[i].first + heapArr[i].size;
-			for(uint32 j = (uint32)virtual_address; j<finalAdd; j+=PAGE_SIZE)
-			{
+	for(int i=0; i<idx; i++) {
+		if (virtual_address == (void*)kHeapArr[i].first) {
+			uint32 finalAdd = kHeapArr[i].first + kHeapArr[i].size;
+			for(uint32 j = (uint32)virtual_address; j<finalAdd; j+=PAGE_SIZE) {
 				unmap_frame(ptr_page_directory, (uint32*)j);
 			}
 
-			heapArr[i].first = heapArr[i].size = 0;
+			kHeapArr[i].first = kHeapArr[i].size = 0;
 		}
 	}
 }
 
-unsigned int kheap_virtual_address(unsigned int physical_address)
-{
+unsigned int kheap_virtual_address(unsigned int physical_address) {
 	//TODO: [PROJECT 2022 - [3] Kernel Heap] kheap_virtual_address()
 	// Write your code here, remove the panic and write your code
 	//panic("kheap_virtual_address() is not implemented yet...!!");
@@ -113,8 +107,7 @@ unsigned int kheap_virtual_address(unsigned int physical_address)
 	return 0;
 }
 
-unsigned int kheap_physical_address(unsigned int virtual_address)
-{
+unsigned int kheap_physical_address(unsigned int virtual_address) {
 	//TODO: [PROJECT 2022 - [4] Kernel Heap] kheap_physical_address()
 	// Write your code here, remove the panic and write your code
 	//panic("kheap_physical_address() is not implemented yet...!!");
