620f7a0e65acffa40b584e9a068ef640e495a9b9
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-05-07 10:03:17 +0200

    Update priority_manager.c

--- FOS_PROJECT_2022_Term2_Template/kern/priority_manager.c
+++ FOS_PROJECT_2022_Term2_Template/kern/priority_manager.c
@@ -4,9 +4,8 @@
 #include <kern/helpers.h>
 #include <kern/user_environment.h>
 
-void set_program_priority(struct Env* env, int priority)
-{
-	//TODO: [PROJECT 2022 - BONUS4] Change WS Size according to Program Priorityù
+void set_program_priority(struct Env* env, int priority) {
+	//TODO: [PROJECT 2022 - BONUS4] Change WS Size according to Program PriorityÔøΩ
 
 	panic("This function is not implemented yet\n");
 
