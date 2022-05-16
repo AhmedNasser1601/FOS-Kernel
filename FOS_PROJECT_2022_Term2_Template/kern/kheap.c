8b3b689611767de7e2dc93ed46625a1dfad20873
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-05-16 06:38:31 +0200

    Update kheap.c
    -Finished: Kmalloc 1 & 2
    -Problem in Kfree <<< run 30% >>>

--- FOS_PROJECT_2022_Term2_Template/kern/kheap.c
+++ FOS_PROJECT_2022_Term2_Template/kern/kheap.c
@@ -2,18 +2,75 @@
 #include <kern/kheap.h>
 #include <kern/memory_manager.h>
 
-#define Mega (1024*1024)
-#define kilo (1024)
-
 //2022: NOTE: All kernel heap allocations are multiples of PAGE_SIZE (4KB)
 
-uint32 startAdd = KERNEL_HEAP_START;
-int idx = 0;
-
 struct KernelHEAP {
-	uint32 first;
-	int size;
-} kHeapArr[Mega];
+	uint32 starter;
+	uint32 pages;
+	int first;
+	int last;
+	uint8 allocated;
+} kHeapArr[(KERNEL_HEAP_MAX-KERNEL_HEAP_START)/PAGE_SIZE];
+
+int x=-1, y=-1;
+int fIDX=0, looping=0;
+int IDX=1, kmallocBEGIN=1, freeBEGIN=1;
+
+uint32 *nextFitAlgo(unsigned int Size) {
+	while(kHeapArr[fIDX].allocated || (kHeapArr[fIDX].pages < Size)) {
+		fIDX = kHeapArr[fIDX].last;
+		if(fIDX == -1) {
+			if(isKHeapPlacementStrategyNEXTFIT() && !looping) {
+				looping = 1;
+				fIDX = 0;
+			}
+			else return NULL;
+		}
+	}
+
+	if(kHeapArr[fIDX].pages != Size) {
+		int lIDX;
+		if(x != -1) {
+			lIDX = x;
+			x = -1;
+		} else if(y != -1) {
+			lIDX = y;
+			y = -1;
+		} else {
+			lIDX = IDX;
+			IDX++;
+		}
+
+		kHeapArr[lIDX].last = kHeapArr[fIDX].last;
+
+		if(kHeapArr[lIDX].last != -1)
+			kHeapArr[kHeapArr[lIDX].last].first = lIDX;
+
+		kHeapArr[lIDX].first = fIDX;
+		kHeapArr[fIDX].last = lIDX;
+
+		kHeapArr[lIDX].allocated = 0;
+		kHeapArr[lIDX].pages = kHeapArr[fIDX].pages - Size;
+		kHeapArr[lIDX].starter = kHeapArr[fIDX].starter + (Size*PAGE_SIZE);
+
+		kHeapArr[fIDX].pages = Size;
+	}
+
+	kHeapArr[fIDX].allocated = 1;
+	uint32 ptrVA = kHeapArr[fIDX].starter;
+
+	for(int i=0; i<Size; i++) {
+		struct Frame_Info* framePTR;
+		if(allocate_frame(&framePTR) != E_NO_MEM) {
+			map_frame(ptr_page_directory, framePTR, (void*)ptrVA, PERM_WRITEABLE|PERM_AVAILABLE);
+			framePTR->va = ptrVA;
+		}
+		ptrVA += PAGE_SIZE;
+	}
+	looping = 0;
+
+	return (void*)kHeapArr[fIDX].starter;
+}
 
 void* kmalloc(unsigned int size) {
 	//TODO: [PROjECT 2022 - [1] Kernel Heap] kmalloc()
@@ -24,34 +81,6 @@ void* kmalloc(unsigned int size) {
 	//NOTE: All kernel heap allocations are multiples of PAGE_SIZE (4KB)
 	//refer to the project presentation and documentation for details
 
-	if(isKHeapPlacementStrategyNEXTFIT()) {
-		uint32 newStart = startAdd;
-		kHeapArr[idx].size = ROUNDUP(size, PAGE_SIZE);
-		uint32 maxSize = startAdd+size;
-
-		if(KERNEL_HEAP_MAX-startAdd > size) {
-			for(uint32 i = startAdd; i < maxSize; i += PAGE_SIZE) {
-				struct Frame_Info* ptrFrameInfo;
-				int tmp = allocate_frame(&ptrFrameInfo);
-				if(tmp == E_NO_MEM) return NULL;
-
-				tmp = map_frame(ptr_page_directory, ptrFrameInfo, (void*)i, PERM_PRESENT|PERM_WRITEABLE);
-				if(tmp == E_NO_MEM) {
-					free_frame(ptrFrameInfo);
-					return NULL;
-				}
-
-				startAdd += PAGE_SIZE;   //Moving Forward
-			}
-
-			kHeapArr[idx].first = newStart;
-
-			idx++;
-			return (void*)newStart;
-		}
-	}
-
-
 	//TODO: [PROjECT 2022 - BONUS1] Implement a Kernel allocation strategy
 	// Instead of the Next allocation/deallocation, implement
 	// BEST FIT strategy
@@ -60,8 +89,20 @@ void* kmalloc(unsigned int size) {
 	//functions to check the current strategy
 	//change this "return" according to your answer
 
+	if(kmallocBEGIN) {
+		kHeapArr[0].starter = KERNEL_HEAP_START;
+		kHeapArr[0].pages = (KERNEL_HEAP_MAX-KERNEL_HEAP_START)/PAGE_SIZE;
+		kHeapArr[0].first = kHeapArr[0].last = -1;
+		kHeapArr[0].allocated = 0;
+		kmallocBEGIN = 0;
+	}
+
+	if(isKHeapPlacementStrategyNEXTFIT()) {
+		return nextFitAlgo(ROUNDUP(size, PAGE_SIZE)/PAGE_SIZE);
+	}
+
 	if(isKHeapPlacementStrategyBESTFIT()) {
-		// BEST FIT strategy
+		// --->>> BONUS -->> BEST FIT -> HERE
 	}
 
 	return NULL;
@@ -75,16 +116,45 @@ void kfree(void* virtual_address) {
 	//you need to get the size of the given allocation using its address
 	//refer to the project presentation and documentation for details
 
-	for(int i=0; i<idx; i++) {
-		if (virtual_address == (void*)kHeapArr[i].first) {
-			uint32 finalAdd = kHeapArr[i].first + kHeapArr[i].size;
-			for(uint32 j = (uint32)virtual_address; j<finalAdd; j+=PAGE_SIZE) {
-				unmap_frame(ptr_page_directory, (uint32*)j);
-			}
+	if(freeBEGIN) {
+		fIDX = 0;
+		freeBEGIN = 0;
+	}
 
-			kHeapArr[i].first = kHeapArr[i].size = 0;
+	int idx = 0;
+	while(idx != -1) {
+		if(kHeapArr[idx].starter == (uint32) virtual_address) break;
+		idx = kHeapArr[idx].last;
+		if(idx == -1) return;
+	}
+
+	kHeapArr[idx].allocated = 0;
+	uint32 pages = kHeapArr[idx].pages;
+
+	if(!isKHeapPlacementStrategyCONTALLOC()) {
+		int first = kHeapArr[idx].first;
+		int last = kHeapArr[idx].last;
+
+		if(last != -1 && !kHeapArr[last].allocated) {
+			kHeapArr[idx].pages += kHeapArr[last].pages;
+			kHeapArr[last].first = idx;
+			kHeapArr[idx].last = kHeapArr[last].last;
+			x = last;
+		}
+
+		first = kHeapArr[idx].first;
+		last = kHeapArr[idx].last;
+
+		if(first != -1 && !kHeapArr[first].allocated) {
+			kHeapArr[first].pages += kHeapArr[idx].pages;
+			kHeapArr[last].first = first;
+			kHeapArr[first].last = last;
+			y = idx;
 		}
 	}
+
+	for(int i=0; i<pages; i++, virtual_address+=PAGE_SIZE)
+		unmap_frame(ptr_page_directory,virtual_address);
 }
 
 unsigned int kheap_virtual_address(unsigned int physical_address) {
@@ -97,15 +167,7 @@ unsigned int kheap_virtual_address(unsigned int physical_address) {
 
 	//change this "return" according to your answer
 
-	struct Frame_Info* phyAddFrame = to_frame_info(physical_address);
-	for(uint32 i = KERNEL_HEAP_START; i < startAdd; i += PAGE_SIZE) {
-		uint32* framePTR = NULL;
-		if(phyAddFrame == get_frame_info(ptr_page_directory,(void*)i,&framePTR)) {
-			return i;
-		}
-	}
-
-	return 0;
+	return frames_info[physical_address>>12].va;
 }
 
 unsigned int kheap_physical_address(unsigned int virtual_address) {
@@ -120,9 +182,6 @@ unsigned int kheap_physical_address(unsigned int virtual_address) {
 
 	uint32 *ptPTR = NULL;
 	get_page_table(ptr_page_directory,(uint32*)virtual_address, &ptPTR);
-
-	if((ptPTR != NULL) && ((ptPTR[PTX(virtual_address)] & PERM_PRESENT) != 0))
-		return ((ptPTR[PTX(virtual_address)] & 0xFFFFF000) + (virtual_address & 0x00000FFF));
-
+	if(ptPTR != NULL) return (ptPTR[PTX(virtual_address)] & 0xFFFFF000);
 	return 0;
 }
